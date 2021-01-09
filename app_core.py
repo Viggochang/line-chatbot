64c773baf11e8ebf1675b1de46d13b45f77ad8cd
shaohua <gl4cj8686@gmail.com>
2021-01-09 20:16:26 +0800

    0109

--- app_core.py
+++ app_core.py
@@ -10,6 +10,7 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSend
 import psycopg2
 import datetime as dt
 import flexmsg
+import cancel
 
 app = Flask(__name__)
 
@@ -52,34 +53,35 @@ def app_core(event):
         print("連接資料庫")
 
         if event.message.text == "~cancel":
-            postgres_select_query=f'''SELECT * FROM group_data WHERE user_id = '{event.source.user_id}' AND condition= 'initial';'''
-            cursor.execute(postgres_select_query)
-            data = cursor.fetchone()
-
-            postgres_select_query=f'''SELECT * FROM registration_data WHERE user_id = '{event.source.user_id}' AND condition= 'initial';'''
-            cursor.execute(postgres_select_query)
-            data_2 = cursor.fetchone()
-
-            postgres_delete_query = f"""DELETE FROM group_data WHERE (condition, user_id) = ('initial', '{event.source.user_id}');"""
-            cursor.execute(postgres_delete_query)
-            conn.commit()
-            postgres_delete_query = f"""DELETE FROM registration_data WHERE (condition, user_id) = ('initial', '{event.source.user_id}');"""
-            cursor.execute(postgres_delete_query)
-            conn.commit()
-
-            cursor.close()
-            conn.close()
-
-            if data or data_2:
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='取消成功')
-                )
-            else:
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='無可取消的開團/報名資料')
-                )
+            cancel.cancel(cursor, event)
+#            postgres_select_query=f'''SELECT * FROM group_data WHERE user_id = '{event.source.user_id}' AND condition= 'initial';'''
+#            cursor.execute(postgres_select_query)
+#            data = cursor.fetchone()
+#
+#            postgres_select_query=f'''SELECT * FROM registration_data WHERE user_id = '{event.source.user_id}' AND condition= 'initial';'''
+#            cursor.execute(postgres_select_query)
+#            data_2 = cursor.fetchone()
+#
+#            postgres_delete_query = f"""DELETE FROM group_data WHERE (condition, user_id) = ('initial', '{event.source.user_id}');"""
+#            cursor.execute(postgres_delete_query)
+#            conn.commit()
+#            postgres_delete_query = f"""DELETE FROM registration_data WHERE (condition, user_id) = ('initial', '{event.source.user_id}');"""
+#            cursor.execute(postgres_delete_query)
+#            conn.commit()
+#
+#            cursor.close()
+#            conn.close()
+#
+#            if data or data_2:
+#                line_bot_api.reply_message(
+#                event.reply_token,
+#                TextSendMessage(text='取消成功')
+#                )
+#            else:
+#                line_bot_api.reply_message(
+#                event.reply_token,
+#                TextSendMessage(text='無可取消的開團/報名資料')
+#                )
 
 
         elif event.message.text == "我要開團":
