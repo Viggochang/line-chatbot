d69333aded4691f5fa1887cc08170bc5575742d9
shaohua <gl4cj8686@gmail.com>
2021-01-22 03:15:33 +0800

    0121

--- app_core.py
+++ app_core.py
@@ -791,6 +791,7 @@ def pic(event):
             #把圖片存下來並傳上去
             ext = 'jpg'
             print(f"messege_id : {event.message.id}")
+            print(event)
             message_content = line_bot_api.get_message_content(event.message.id)
             with tempfile.NamedTemporaryFile(dir = static_tmp_path, prefix = ext + '-', delete = False) as tf:
                 for chunk in message_content.iter_content():
