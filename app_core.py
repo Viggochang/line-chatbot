3b54ac5e3dadbe92eb12471960e0268f11d795a8
shaohua <gl4cj8686@gmail.com>
2021-01-25 00:33:52 +0800

    0122

--- app_core.py
+++ app_core.py
@@ -783,8 +783,12 @@ def handle_message(event):
     if isinstance(event.message, ImageMessage):
         ext = 'jpg'
         print(event)
+        
+        config = configparser.ConfigParser()
+        config.read('config.ini')
         line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
         message_content = line_bot_api.get_message_content(event.message.id)
+        
         with tempfile.NamedTemporaryFile(dir = static_tmp_path, prefix=ext + '-', delete=False) as tf:
             for chunk in message_content.iter_content():
                 tf.write(chunk)
