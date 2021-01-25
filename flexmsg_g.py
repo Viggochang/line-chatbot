e4a4468edb55c00b11a370d9b9595ddc772c6ff9
shaohua <gl4cj8686@gmail.com>
2021-01-26 02:33:46 +0800

    0122

--- flexmsg_g.py
+++ flexmsg_g.py
@@ -396,7 +396,7 @@ def name(progress):
             footer=BoxComponent(
                 layout = "vertical",
                 margin = "md",
-                contents = [TextComponent(text = f"{progress[5]} / {progress[0]} ", weight = "bold", size = "md"),
+                contents = [TextComponent(text = f"{progress[6]} / {progress[0]} ", weight = "bold", size = "md"),
                             BoxComponent(layout = "vertical",
                                          margin = "md",
                                          contents = [
@@ -404,7 +404,7 @@ def name(progress):
                                                           contents = [FillerComponent()]
                                                          )
                                          ],
-                                         width = f"{int(progress[5] / progress[0] * 100 + 0.5 )}%",
+                                         width = f"{int(progress[6] / progress[0] * 100 + 0.5 )}%",
                                          background_color = "#3DE1D0",
                                          height = "6px"
                                         )
@@ -435,7 +435,7 @@ def phone(progress):
             footer = BoxComponent(
                 layout = "vertical",
                 margin = "md",
-                contents = [TextComponent(text = f"{progress[6]} / {progress[0]} ", weight = "bold", size = "md"),
+                contents = [TextComponent(text = f"{progress[7]} / {progress[0]} ", weight = "bold", size = "md"),
                             BoxComponent(layout = "vertical",
                                          margin = "md",
                                          contents = [
@@ -443,7 +443,7 @@ def phone(progress):
                                                           contents = [FillerComponent()]
                                                          )
                                          ],
-                                         width = f"{int(progress[6] / progress[0] * 100 + 0.5 )}%",
+                                         width = f"{int(progress[7] / progress[0] * 100 + 0.5 )}%",
                                          background_color = "#3DE1D0",
                                          height = "6px"
                                         )
