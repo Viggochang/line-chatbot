b10a9862eb6f9abda28a56124f34721f4f6c889b
shaohua <gl4cj8686@gmail.com>
2021-01-27 00:17:16 +0800

    0126

--- app_core.py
+++ app_core.py
@@ -863,44 +863,40 @@ def pic(event):
             dist_name = os.path.basename(dist_path)
             print(f"dist_path={dist_path}", "\n", f"dist_name={dist_name}")
 
-            #try:
-            config = configparser.ConfigParser()
-            config.read('config.ini')
-            client = ImgurClient(config.get('imgur', 'client_id'), config.get('imgur', 'client_secret'), config.get('imgur', 'access_token'), config.get('imgur', 'refresh_token'))
-            con = {
-                'album': config.get('imgur', 'album_id'),
-                'name': f'{event.source.user_id}_{data_g[3]}',
-                'title': f'{event.source.user_id}_{data_g[3]}',
-                'description': f'{event.source.user_id}_{data_g[3]}'
-            }
+            try:
+                config = configparser.ConfigParser()
+                config.read('config.ini')
+                client = ImgurClient(config.get('imgur', 'client_id'), config.get('imgur', 'client_secret'), config.get('imgur', 'access_token'), config.get('imgur', 'refresh_token'))
+                con = {
+                    'album': config.get('imgur', 'album_id'),
+                    'name': f'{event.source.user_id}_{data_g[3]}',
+                    'title': f'{event.source.user_id}_{data_g[3]}',
+                    'description': f'{event.source.user_id}_{data_g[3]}'
+                }
 
-            image = client.upload_from_path(dist_path, config = con, anon = False)
-            os.remove(dist_path)
-            print("image = ",image)
-            #把圖片網址存進資料庫
-            postgres_update_query = f"""UPDATE group_data SET photo = '{image['link']}' WHERE condition = 'initial' AND user_id = '{event.source.user_id}';"""
-            cursor.execute(postgres_update_query)
-            conn.commit()
-            
-            print(image['link'])
-            msg = [TextSendMessage(text='上傳成功')]
+                image = client.upload_from_path(dist_path, config = con, anon = False)
+                os.remove(dist_path)
+                print("image = ",image)
+                #把圖片網址存進資料庫
+                postgres_update_query = f"""UPDATE group_data SET photo = '{image['link']}' WHERE condition = 'initial' AND user_id = '{event.source.user_id}';"""
+                cursor.execute(postgres_update_query)
+                conn.commit()
+                
+                print(image['link'])
 
-            postgres_select_query = f"""SELECT * FROM group_data WHERE condition = 'initial' AND user_id = '{event.source.user_id}';"""
-            cursor.execute(postgres_select_query)
-            data_g = cursor.fetchone()
-            
-            #msg.append(flexmsg_g.summary(data_g))
-            msg = [TextSendMessage(text='上傳成功'), flexmsg_g.summary(data_g)]
+                postgres_select_query = f"""SELECT * FROM group_data WHERE condition = 'initial' AND user_id = '{event.source.user_id}';"""
+                cursor.execute(postgres_select_query)
+                data_g = cursor.fetchone()
+                
+                msg = [TextSendMessage(text='上傳成功'), flexmsg_g.summary(data_g)]
 
+            except:
+                msg = [TextSendMessage(text='上傳失敗'), flexmsg_g.summary(data_g)]
+                
             line_bot_api.reply_message(
                 event.reply_token,
                 msg
             )
-
-#            except:
-#                line_bot_api.reply_message(
-#                    event.reply_token,
-#                    TextSendMessage(text='上傳失敗'))
 
     else:
         line_bot_api.reply_message(
