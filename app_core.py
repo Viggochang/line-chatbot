0588a0677b8e42d3a5bbe8cfae98cf882385e70c
shaohua <gl4cj8686@gmail.com>
2021-02-21 17:53:25 +0800

    0221

--- app_core.py
+++ app_core.py
@@ -56,7 +56,7 @@ def from_start():
 def show_groupdata():
     all_groupdata = CallDatabase.get_group_data()
     print(len(all_groupdata))
-    return render_template("group.html")
+    return render_template("group.html", html_data = all_groupdata)
 
 
 # 學你說話
