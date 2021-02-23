6032f194efa1641862892ba8228253fc63ac9e60
shaohua <gl4cj8686@gmail.com>
2021-02-23 21:41:51 +0800

    0222

--- custom_models/CallDatabase.py
+++ custom_models/CallDatabase.py
@@ -22,11 +22,19 @@ def filter_group(form):
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
     
-    activity_type = form["activity_type"]
-    cost_min = form["cost_min"]
-    cost_max = form["cost_max"]
+    activity_type, cost_min, cost_max = form["activity_type"], form["cost_min"], form["cost_max"]
+    condition_query = []
+    if activity_type:
+        condition_query.append(f"activity_type = '{activity_type}'")
+    if cost_min:
+        condition_query.append(f"cost >= {cost_min}")
+    if cost_max:
+        condition_query.append(f"cost <= {cost_max}")
     
-    select_query = f'''SELECT activity_type, activity_name, activity_date, activity_time, cost FROM group_data WHERE activity_type = '{activity_type}' AND cost >= {cost_min} AND cost <= {cost_max}'''
+    if condition_query:
+        select_query = '''SELECT activity_type, activity_name, activity_date, activity_time, cost FROM group_data''' + ''' WHERE ''' + ''' AND '''.join(condition_query) + ''' ORDER BY activity_date'''
+    else:
+        select_query = '''SELECT activity_type, activity_name, activity_date, activity_time, cost FROM group_data ORDER BY activity_date'''
     
     cursor.execute(select_query)
     conn.commit()
