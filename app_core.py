ae5086f263c88ca0d9e5dce908d74f7808fa427e
shao <gl4cj8686@gmail.com>
2021-03-13 17:23:17 +0800

    0313

--- app_core.py
+++ app_core.py
@@ -173,7 +173,11 @@ def group():
                 image = client.upload_from_path(dist_path, config = con, anon = False)
                 os.remove(dist_path)
                 print("image = ",image)
+                print("上傳成功")
                 
+            except:
+                print("上傳失敗")
+        
         q = ["""condition = 'pending'"""]
         for g_col in request.form:
             if request.form[g_col]:
