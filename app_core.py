3d3970fc2e5f88d343b1c51b9e6c4576ab9956fb
shao <gl4cj8686@gmail.com>
2021-04-26 19:57:09 +0800

    use CallDatabase

--- app_core.py
+++ app_core.py
@@ -1140,8 +1140,13 @@ def gathering(event):
 
 @handler.add(MessageEvent, message = ImageMessage)
 def pic(event):
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode = 'require')
+    cursor = conn.cursor()
+
     condition = {"condition": ["=", "initial"], "user_id": ["=", event.source.user_id]}
     data_g = CallDatabase.get_data("group_data", condition = condition, all_data = False)
+
     if data_g:
         i = data_g.index(None)
         print("i =",i)
