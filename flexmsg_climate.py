8cff0c27eeb7d4cbc589fa47a3b4076e09b1b06d
shao <gl4cj8686@gmail.com>
2021-05-02 02:20:19 +0800

    climate

--- flexmsg_climate.py
+++ flexmsg_climate.py
@@ -3,7 +3,7 @@ from flask import url_for
 
 def climate(activity_date, county, district, rain, weather, temperature_avg, temperature_max, temperature_min, humidity, wind_d, wind_v, uvi):
     rain_prob = f"降雨機率:{rain}%" if rain != " " else "無降雨機率資料"
-    weather_size = "lg" if len(weather) > 4 else "xl"
+    weather_size = "lg" if len(weather) > 4 else "27px"
 
     if "晴" in weather and "雨" in weather:
         image = "https://i.imgur.com/jM4qYAq.png"
